74cbaba7d5eee2d63793911445111773c5fb1036
AV <alessandro.vitelli78@gmail.com>
2024-12-19 20:52:46 +0100

    change amount

--- js/membership-form.js
+++ js/membership-form.js
@@ -275,7 +275,7 @@ document.addEventListener('DOMContentLoaded', async function() {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({
-                        amount: 0.50,
+                        amount: 1.05,
                         currency: 'EUR',
                         checkout_reference: `membership-${Date.now()}`,
                         merchant_code: SUMUP_CONFIG.merchant_code,
